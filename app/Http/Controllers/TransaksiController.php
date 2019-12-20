70fe1b0e6036ee37c6af70239a3f710dfd205ca7
alfianrehanusa <alfianrehanusa@eklankuteams.com>
2019-12-20 14:34:05 +0700

    get data from report_transaksi by date

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -4,7 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\ReportTransaksi;
+
 class TransaksiController extends Controller
 {
-    //
+    public function index(){
+        $reportTransaksi = ReportTransaksi::selectRaw('to_char(tgl_trx, \'YYYY-MM-DD\') AS date, count(to_char(tgl_trx, \'YYYY-MM-DD\')) AS total_transaksi')
+        ->groupBy('date')
+        ->orderBy('date', 'DESC')
+        ->get();
+
+        dd($reportTransaksi);
+    }
 }
---
+++ app/ReportTransaksi.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ReportTransaksi extends Model
+{
+    protected $table = 'report_transaksi';
+    public $timestamps = false;
+}
