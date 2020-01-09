e76e65a3f44dd72780c6ec41402e5031567d8127
alfianrehanusa <alfianrehanusa@eklankuteams.com>
2020-01-09 08:38:53 +0700

    new

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -18,9 +18,9 @@ class TransaksiController extends Controller
         $hari_lalu = '52';
 
         $cekData = ReportTrxCountPerday::whereRaw('EXTRACT(day FROM date) = ' . date('d', strtotime("-" . $hari_lalu . " days")) .
-        'AND EXTRACT(month FROM date) = ' . date('m', strtotime("-" . $hari_lalu . " days")) .
-        'AND EXTRACT(year FROM date) = ' . date('Y', strtotime("-" . $hari_lalu . " days")))
-        ->first();
+            'AND EXTRACT(month FROM date) = ' . date('m', strtotime("-" . $hari_lalu . " days")) .
+            'AND EXTRACT(year FROM date) = ' . date('Y', strtotime("-" . $hari_lalu . " days")))
+            ->first();
         if(!$cekData){
             $getData = ReportTransaksi::selectRaw('to_char(tgl_trx, \'YYYY-MM-DD\') AS date,
                     count(to_char(tgl_trx, \'YYYY-MM-DD\')) AS total_transaksi')
